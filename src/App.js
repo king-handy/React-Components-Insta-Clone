f58b6e522e700310ce3b2c4b25247e9673b4051c
king-handy <a.king.life@pm.me>
2021-06-20 22:11:17 -0400

    Apps start

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-  const posts = useState()
+  const post = useState({dummyData})
 
   const likePost = postId => {
     /*
